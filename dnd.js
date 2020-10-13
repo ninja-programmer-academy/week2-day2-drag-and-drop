e97e15d8771489e74ba0aaee6c6b028616db2c58
= <alex@ninjasoftware.com.au>
2020-10-13 13:37:56 +0800

    ghost box implementation

--- dnd.js
+++ dnd.js
@@ -13,24 +13,47 @@ const draggables = document.querySelectorAll('#boxes .box');
 //get the box container
 const container = document.getElementById('boxes');
 
-//loop over each box and give it drag events
-draggables.forEach(draggable => {
-    draggable.addEventListener('dragstart', () => {
-        //give our box that is currently being dragged over a class to help us find it later
-        draggable.classList.add('dragging');
+//initialise our drag and drop engine
+function initDND() {
+    //create a ghost box div for representing where our box will end up
+    let ghostBox = document.createElement("div");
+    ghostBox.className = "ghost box";
+
+    //loop over each box and give it drag events
+    draggables.forEach(draggable => {
+        draggable.addEventListener('dragstart', () => {
+            //give our box that is currently being dragged over a class to help us find it later
+            ghostBox.style.backgroundColor = draggable.style.backgroundColor;
+            draggable.classList.add('dragging');
+        })
+
+        draggable.addEventListener('dragend', () => {
+            //remove dragging class
+            draggable.classList.remove('dragging');
+            if (ghostBox.parentNode) {
+                ghostBox.parentNode.removeChild(ghostBox);
+            }
+        })
+
+
+        draggable.addEventListener('dragover', e => {
+            e.preventDefault();
+            const dragging = document.querySelector('.dragging');
+
+            if (dragging === draggable) { return }
+
+            //use getBoundingClientRect() to get box underneath our dragging
+            let rect = e.currentTarget.getBoundingClientRect();
+            let x = e.clientX - rect.left; //x position within the element.
+
+            //determine whether x position is more left than right and insert ghost box appropriately
+            if (x < (rect.width/2)) {
+                draggable.parentNode.insertBefore(ghostBox, draggable);
+            } else {
+                draggable.parentNode.insertBefore(ghostBox, draggable.nextSibling);
+            }
+        })
     })
+}
 
-    draggable.addEventListener('dragend', () => {
-        //remove dragging class
-        draggable.classList.remove('dragging');
-    })
-
-
-    draggable.addEventListener('dragover', e => {
-        e.preventDefault();
-        const dragging = document.querySelector('.dragging');
-
-        //console.log the box being dragged and the box being hovered over
-        console.log(dragging, draggable);
-    })
-})
+initDND();
